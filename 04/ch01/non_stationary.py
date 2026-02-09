1736f7a09b82afb56799a6f845cf2184d0014240
Cyril <sirisiri.mori@gmail.com>
2026-02-09 23:49:08 +0900

    非定常問題に対応したエージェントクラスを実装

--- 04/ch01/non_stationary.py
+++ 04/ch01/non_stationary.py
@@ -12,3 +12,19 @@ class NonStatBandit:
             return 1
         else:
             return 0
+
+class AlphaAgent:
+    def __init__(self, epsilon, alpha, actions = 10):
+        self.epsilon = epsilon
+        self.Qs = np.zeros(actions)
+        self.alpha = alpha
+    
+    def update(self, action, reward):
+        # alpha で更新
+        self.Qs[action] += (reward - self.Qs[action]) * self.alpha
+        # self.Qs[action] += (reward - self.Qs[action]) / self.ns[action]   # 割る数が実施回数に依存している
+
+    def get_action(self):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(0, len(self.Qs))
+        return np.argmax(self.Qs)
