604d425b34695f52cfd25da61b61704e253affb3
Cyril <sirisiri.mori@gmail.com>
2026-02-22 22:01:45 +0900

    `im2col`メソッドの理解に苦しみました。

--- 01/common/util.py
+++ 01/common/util.py
@@ -22,14 +22,18 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
 
     # 各フィルターの要素（マス目）を二つのfor文で走らせる。
+    # y_max, x_maxはそれぞれy, xの移動する最大の位置（ゴール）
     for y in range(filter_h):
         y_max = y + stride*out_h
         for x in range(filter_w):
             x_max = x + stride*out_w
             col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
 
+    # colの場合、（transposeした後）reshapeメソッドはcol[0, 0, 0, 0, 0, 1]から順に並べていき、その並べたものから順にピックアップしていくことで、reshapeで指定した形状の配列を生成する。
+    # 今回は、colの第1, 2, 3次元目をすべて1行（横向き）にフラット化したもの縦に重ねていく。
     col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
     return col
+    # col.transpose(N, out_h, out_w, channel, H, W)
 
 
 def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
