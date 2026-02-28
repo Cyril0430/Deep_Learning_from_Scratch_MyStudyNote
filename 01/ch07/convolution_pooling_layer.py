4f809fb9ec917ddd2ac094c4b50eaa5b4b18972f
Cyril <sirisiri.mori@gmail.com>
2026-02-28 13:19:10 +0900

    Poolingレイヤの実装をやった。次はプーリング対象となる入力データ（パディング済み）の一部を横（行方向）に並べ、縦が「channel * padded_out_w * padded_out_h * N」（実装におけるかけ算は「掛ける数 * 掛けられる数」であることに注意）になるようになっている点について深堀する。まぁ、コードを吟味するところから行いましょう。

--- 01/ch07/convolution_pooling_layer.py
+++ 01/ch07/convolution_pooling_layer.py
@@ -72,3 +72,21 @@ class Convolution:
         dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
 
         return dx
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride=2, pad=0):
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        # 展開 (1)
+
+        # ↓このコードの吟味から（処理の流れを思い浮かべよう）
+        col = im2col(x, self.pool_h, self.pool_w, self.stride, self.pad)    # col: (N*padded_out_h*padded_out_w, C*self.pool_h*self.pool_w)
+        col = col.reshape(-1, self.pool_h*self.pool_w)  # (N*padded_out_h*padded_out_w*C, self.pool_h*self.pool_w)
