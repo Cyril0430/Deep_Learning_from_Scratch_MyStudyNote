8c35899cd53d995e0191f3934f9826847f919133
Cyril <sirisiri.mori@gmail.com>
2026-03-06 12:50:16 +0900

    new-item: 04/common/gridworld.py
    教科書P97から掲載されているコードの実装

---
+++ 04/common/gridworld.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class GridWorld:
+    """3×4マスのグリッドワールド
+    
+    インスタンス
+    """
+    def __init__(self):
+        self.action_space = [0, 1, 2, 3]
+        self.action_meaning = {
+            0: "UP",
+            1: "DOWN",
+            2: "LEFT",
+            3: "RIGHT",
+        }
+
+        self.reward_map = np.array(
+            [[0, 0, 0, 1.0],
+             [0, None, 0, -1.0],
+             [0, 0, 0, 0]]
+        )
+        self.goal_state = (0, 3)
+        self.wall_state = (1, 1)
+        self.start_state = (2, 0)
+        self.agent_state = self.start_state
