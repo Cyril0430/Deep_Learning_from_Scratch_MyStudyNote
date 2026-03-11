54b0a3228cad8e5fbc4d3f2a25c2d070df878d25
Cyril <sirisiri.mori@gmail.com>
2026-03-12 00:21:21 +0900

    update: ./04/ch04/4.2.ipynb and 04/common/gridworld.py
    3×4マスグリッドワールドの実装の途中まで

--- 04/common/gridworld.py
+++ 04/common/gridworld.py
@@ -1,4 +1,5 @@
 import numpy as np
+import common.gridworld_render as render_helper
 
 class GridWorld:
     """3×4マスのグリッドワールド
@@ -36,10 +37,37 @@ class GridWorld:
     def shape(self):
         return self.reward_map.shape    # self.reward_mapの形状（`(3, 4)`）
     
-    def action(self):
+    def action(self):   # 全ての行動にアクセス
         return self.action_space    # [0, 1, 2, 3]
     
-    def states(self):
+    def states(self):   # 全ての状態にアクセス（一回の処理毎に出力する）
         for h in range(self.height):    # ここで、メソッドを`self.height`というインスタンス変数として使用している！
             for w in range(self.width): # ここでも！
-                yield (h, w)
+                yield (h, w)            # yieldを用いることで、一回の実行毎にfor文で得られる結果を返すことができる。
+
+    # 環境の状態遷移を表すメソッドと報酬関数のメソッドを実装
+    
+    def next_state(self, state, action):    # 環境の状態遷移を表すメソッド
+        # 1. 移動先の場所の計算
+        action_move_map = [(-1, 0), (1, 0), (0, -1), (0, 1)]    # 場所の変化量（左から「DOWN」「UP」「LEFT」「RIGHT」。数学の座標とはxとyが逆なので注意！）
+        move = action_move_map[action]  # どちらに動くか決定（左右上下のいずれか）
+        next_state = (state[0] + move[0], state[1] + move[1])   # 次の状態に遷移する（y軸, x軸）
+        ny, nx = next_state # 次の状態のx座標（nx）とy座標（ny）が設定される。
+
+        # 2. 移動先がグリッドワールドの枠の外か、それとも移動先が壁か？
+        if nx < 0 or nx >= self.width or ny < 0 or ny >= self.height:   # 次の状態が枠外のとき
+            next_state = state  # 状態をもとに戻す
+        elif next_state == self.wall_state: # 次の状態が壁のとき
+            next_state = state  # 状態をもとに戻す
+        
+        # 3. 次の状態を返す
+        return next_state   # 今回は状態遷移が決定論的なので、そのまま（確率1で）次の状態を返す。
+    
+    def reward(self, state, action, next_state):    # 報酬関数を表すメソッド
+        return self.reward_map[next_state]  # この本では報酬関数を決定論的なものとみなしているので、報酬をそのまま返す。しかし今回は状態遷移が決定論的であるため、次の状態のみによって報酬を決定している点に注意。
+    
+    # 次の関数はグリッドを可視化するものであり、メソッドの定義の中身はあまり重要ではない
+    def render_v(self, v=None, policy=None, print_value=True):
+        renderer = render_helper.Renderer(self.reward_map, self.goal_state,
+                                          self.wall_state)
+        renderer.render_v(v, policy, print_value)
