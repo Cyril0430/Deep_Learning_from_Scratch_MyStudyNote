e0097d47be5b6b5d9a94fb22257e98045faa26cd
Cyril <sirisiri.mori@gmail.com>
2026-03-25 14:18:42 +0900

    McAgentクラスの実装が完了。吟味も同時に行えるようになってきた。

--- 04/ch05/mc_eval.py
+++ 04/ch05/mc_eval.py
@@ -33,7 +33,7 @@ class RandomAgent:
             self.cnts[state] += 1
             self.V[state] += (G - self.V[state]) / self.cnts[state]
     
-def greedy_probs(Q, state, action_size=4):
+def pre_greedy_probs(Q, state, action_size=4):
     qs = [Q[(state, action)] for action in range(action_size)]
     max_action = np.argmax(qs)
 
@@ -42,7 +42,17 @@ def greedy_probs(Q, state, action_size=4):
     action_probs[max_action] = 1
     return action_probs
 
-class McAgent:
+def greedy_probs(Q, state, epsilon=0, action_size=4):
+    qs = [Q[(state, action)] for action in range(action_size)]
+    max_action = np.argmax(qs)
+
+    base_prob = epsilon / action_size
+    action_probs = {action: base_prob for action in range(action_size)}
+    # ↑この時点では{0: ε/4, 1: ε/4, 2: ε/4, 3: ε/4}
+    action_probs[max_action] += (1 - epsilon)
+    return action_probs
+
+class PreMcAgent:
     def __init__(self):
         self.gamma = 0.9
         self.action_size = 4
@@ -75,4 +85,38 @@ class McAgent:
             self.cnts[key] += 1 # そっか、行動価値関数は状態と行動の二変数関数だから、その二つの値のタプルがキーになるようにして行動価値関数に合わせているのか。
             self.Q[key] += (G - self.Q[key]) / self.cnts[key]
 
-            self.pi[state] = greedy_probs(self.Q, state)
+            self.pi[state] = pre_greedy_probs(self.Q, state)
+
+class McAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.epsilon = 0.1  # epsilon-greedyのepsilon
+        self.alpha = 0.1    # 行動価値関数の値を更新する際の固定値alpha
+        self.aciton_size = 4
+
+        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
+        self.pi = defaultdict(lambda: random_actions)
+        self.Q = defaultdict(lambda: 0)
+        self.memory = []
+
+    def get_action(self, state):
+        action_probs = self.pi[state]
+        actions = list(action_probs.keys())
+        probs = list(action_probs.values())
+        return np.random.choice(actions, p=probs)
+    
+    def add(self, state, action, reward):
+        data = (state, action, reward)
+        self.memory.append(data)
+    
+    def reset(self):
+        self.memory.clear()
+    
+    def update(self):
+        G = 0
+        for data in reversed(self.memory):
+            state, action, reward = data
+            G = self.gamma * G + reward
+            key = (state, action)
+            self.Q[key] += (G - self.Q[key]) * self.alpha   # 1. 固定値
+            self.pi[state] = greedy_probs(self.Q, state, self.epsilon)
