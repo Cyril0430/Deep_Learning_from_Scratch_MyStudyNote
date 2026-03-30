f0531b9dade29dc4dd132c39a48321aa834c53f7
Cyril <sirisiri.mori@gmail.com>
2026-03-30 10:03:28 +0900

    Q学習を実装した

---
+++ 04/ch06/q_learning.py
@@ -0,0 +1,38 @@
+import sys, os
+sys.path.append(os.pardir)
+
+from collections import defaultdict
+import numpy as np
+from common.gridworld import GridWorld
+from common.utils import greedy_probs
+
+class QLearningAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.alpha = 0.8
+        self.epsilon = 0.1
+        self.action_size = 4
+
+        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
+        self.pi = defaultdict(lambda: random_actions)
+        self.b = defaultdict(lambda: random_actions)    # 挙動方策
+        self.Q = defaultdict(lambda: 0)
+
+    def get_action(self, state):
+        action_probs = self.b[state]    # 挙動方策によって行動を取得
+        actions = list(action_probs.keys())
+        probs = list(action_probs.values())
+        return np.random.choice(actions, p=probs)
+    
+    def update(self, state, action, reward, next_state, done):
+        if done:
+            next_q_max = 0
+        else:
+            next_q = [self.Q[next_state, a] for a in range(self.action_size)]
+            next_q_max = max(next_q)
+        
+        target = reward + self.gamma * next_q_max
+        self.Q[state, action] += (target - self.Q[state, action]) * self.alpha
+
+        self.pi[state] = greedy_probs(self.Q, state, epsilon=0)
+        self.b[state] = greedy_probs(self.Q, state, epsilon=self.epsilon)
