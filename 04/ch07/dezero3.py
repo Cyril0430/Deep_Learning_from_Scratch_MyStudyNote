090c83cb1234351b9c83b22865b7ff6e778d77ce
Cyril <sirisiri.mori@gmail.com>
2026-03-30 14:18:51 +0900

    DeZeroを用いて線形回帰問題の損失関数を最適化した

--- 04/ch07/dezero3.py
+++ 04/ch07/dezero3.py
@@ -14,3 +14,28 @@ b = Variable(np.zeros(1))
 def predict(x):
     y = F.matmul(x, W) + b
     return y
+
+def mean_squared_error(x0, x1):
+    diff = x0 - x1
+    return F.sum(diff**2) / len(diff)
+
+lr = 0.1
+iters = 100
+
+for i in range(iters):
+    y_pred = predict(x)
+    loss = mean_squared_error(y, y_pred)
+    # もしくは loss = F.mean_squared_error(y, y_pred)
+    W.cleargrad()
+    b.cleargrad()
+    loss.backward()
+
+    W.data -= lr * W.grad.data
+    b.data -= lr * b.grad.data
+
+    if i % 10 == 0: # 10回ごとに出力
+        print(loss.data)
+
+print("===")
+print(f"W = {W.data}")
+print(f"b = {b.data}")
